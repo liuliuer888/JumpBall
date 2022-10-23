c4df1a30afd93f43268a852f840d71ea04a3536b
刘志涛 <522352339@qq.com>
2022-10-23 16:58:25 +0800

    提交代码

--- JumpBall/Assets/scripts/battle/HitTrigger.cs
+++ JumpBall/Assets/scripts/battle/HitTrigger.cs
@@ -6,9 +6,21 @@ using System;
 public class HitTrigger : MonoBehaviour 
 {
     public Action<Collision2D> OnHit;
+    public Action<Collision2D> OnExitHit;
 
     void OnCollisionEnter2D(Collision2D co)
 	{
-        OnHit (co);
+        if (OnHit != null)
+        {
+            OnHit(co);
+        }
 	}
+
+    void OnCollisionExit2D(Collision2D co)
+    {
+        if(OnExitHit != null)
+        {
+            OnExitHit(co);
+        }
+    }
 }
